b2443ea7d1e9504ee6b8e3cdace251dc41fd5c96
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-04-01 17:38:08 +0530

    print chat/user id at auth/unauth

--- bot/modules/authorize.py
+++ bot/modules/authorize.py
@@ -17,18 +17,18 @@ def authorize(update, context):
             if chat_id not in AUTHORIZED_CHATS:
                 file.write(f"{chat_id}\n")
                 AUTHORIZED_CHATS.add(chat_id)
-                msg = "Chat authorized"
+                msg = f"Chat authorized: <i>{chat_id}</i>"
             else:
-                msg = "Already authorized chat"
+                msg = f"Already authorized chat: <i>{chat_id}</i>"
         else:
             # Trying to authorize someone in specific
             user_id = reply_message.from_user.id
             if user_id not in AUTHORIZED_CHATS:
                 file.write(f"{user_id}\n")
                 AUTHORIZED_CHATS.add(user_id)
-                msg = "Person Authorized to use the bot!"
+                msg = f"Person Authorized to use the bot!: <i>{user_id}</i>"
             else:
-                msg = "Person already authorized"
+                msg = f"Person already authorized: <i>{user_id}</i>"
         sendMessage(msg, context.bot, update)
 
 
@@ -40,17 +40,17 @@ def unauthorize(update, context):
         chat_id = update.effective_chat.id
         if chat_id in AUTHORIZED_CHATS:
             AUTHORIZED_CHATS.remove(chat_id)
-            msg = "Chat unauthorized"
+            msg = f"Chat unauthorized: <i>{chat_id}</i>"
         else:
-            msg = "Already unauthorized chat"
+            msg = f"Already unauthorized chat: <i>{chat_id}</i>"
     else:
         # Trying to authorize someone in specific
         user_id = reply_message.from_user.id
         if user_id in AUTHORIZED_CHATS:
             AUTHORIZED_CHATS.remove(user_id)
-            msg = "Person unauthorized to use the bot!"
+            msg = f"Person unauthorized to use the bot!: <i>{user_id}</i>"
         else:
-            msg = "Person already unauthorized!"
+            msg = f"Person already unauthorized!: <i>{user_id}</i>"
     with open("authorized_chats.txt", "a") as file:
         file.truncate(0)
         for i in AUTHORIZED_CHATS:
