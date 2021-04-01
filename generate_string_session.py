bc9aa7b5cb5d2ff56a3bc1d6797408781a0eceb9
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-04-01 15:35:00 +0530

    try to load vars from config.env in generate_string_session

--- generate_string_session.py
+++ generate_string_session.py
@@ -1,6 +1,24 @@
+from os import environ
+
+from dotenv import load_dotenv
 from pyrogram import Client
 
-API_KEY = int(input("Enter API KEY: "))
-API_HASH = input("Enter API HASH: ")
+# try to load vars from config.env
+load_dotenv("config.env")
+try:
+    API_KEY = environ["TELEGRAM_API"]
+    if len(API_KEY) == 0:
+        API_KEY = int(input("Enter API KEY: "))
+except KeyError:
+    API_KEY = int(input("Enter API KEY: "))
+
+try:
+    API_HASH = environ["TELEGRAM_HASH"]
+    if len(API_HASH) == 0:
+        API_HASH = int(input("Enter API KEY: "))
+except KeyError:
+    API_HASH = int(input("Enter API KEY: "))
+
+
 with Client(":memory:", api_id=API_KEY, api_hash=API_HASH) as app:
     print(app.export_session_string())
