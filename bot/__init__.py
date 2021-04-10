df78be489518b3c5cb84818dbdb7a77102e2a8b0
KenHV <yo@kenharris.xyz>
2021-04-10 17:00:26 +0530

    init: fix trailing backslash check for DOWNLOAD_DIR
    the previous check always results in True.
    
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- bot/__init__.py
+++ bot/__init__.py
@@ -68,8 +68,8 @@ try:
     BOT_TOKEN = getConfig("BOT_TOKEN")
     parent_id = getConfig("GDRIVE_FOLDER_ID")
     DOWNLOAD_DIR = getConfig("DOWNLOAD_DIR")
-    if DOWNLOAD_DIR[-1] != "/" or DOWNLOAD_DIR[-1] != "\\":
-        DOWNLOAD_DIR = DOWNLOAD_DIR + "/"
+    if not DOWNLOAD_DIR.endswith("/"):
+        DOWNLOAD_DIR = DOWNLOAD_DIR + '/'
     DOWNLOAD_STATUS_UPDATE_INTERVAL = int(getConfig("DOWNLOAD_STATUS_UPDATE_INTERVAL"))
     OWNER_ID = int(getConfig("OWNER_ID"))
     AUTO_DELETE_MESSAGE_DURATION = int(getConfig("AUTO_DELETE_MESSAGE_DURATION"))
