978d9278ef912750eb982bbc102136f5c0370cbc
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-04-15 16:26:11 +0530

    init: auto generate session string and refactor

--- bot/__init__.py
+++ bot/__init__.py
@@ -8,36 +8,34 @@ import time
 import aria2p
 import telegram.ext as tg
 from dotenv import load_dotenv
-
-faulthandler.enable()
-
-socket.setdefaulttimeout(600)
-
-botStartTime = time.time()
-if os.path.exists("log.txt"):
-    with open("log.txt", "r+") as f:
-        f.truncate(0)
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
-)
-
-load_dotenv("config.env")
-
-Interval = []
+from pyrogram import Client
 
 
 def getConfig(name: str):
     return os.environ[name]
 
 
+faulthandler.enable()
+socket.setdefaulttimeout(600)
+
+try:
+    with open("log.txt", "r+") as f:
+        f.truncate(0)
+except FileNotFoundError:
+    pass
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
 LOGGER = logging.getLogger(__name__)
+
+load_dotenv("config.env")
 
 try:
     if bool(getConfig("_____REMOVE_THIS_LINE_____")):
-        logging.error("The README.md file is there to be read! Exiting now!")
+        LOGGER.error("The README.md file is there to be read! Exiting now!")
         exit()
 except KeyError:
     pass
@@ -53,6 +51,9 @@ aria2 = aria2p.API(
 DOWNLOAD_DIR = None
 BOT_TOKEN = None
 
+botStartTime = time.time()
+Interval = []
+
 download_dict_lock = threading.Lock()
 status_reply_dict_lock = threading.Lock()
 # Key: update.effective_chat.id
@@ -63,6 +64,7 @@ status_reply_dict = {}
 download_dict = {}
 
 try:
+    LOGGER.info("Loading environment variables...")
     BOT_TOKEN = getConfig("BOT_TOKEN")
     parent_id = getConfig("GDRIVE_FOLDER_ID")
     DOWNLOAD_DIR = getConfig("DOWNLOAD_DIR")
@@ -71,12 +73,17 @@ try:
     DOWNLOAD_STATUS_UPDATE_INTERVAL = int(getConfig("DOWNLOAD_STATUS_UPDATE_INTERVAL"))
     OWNER_ID = int(getConfig("OWNER_ID"))
     AUTO_DELETE_MESSAGE_DURATION = int(getConfig("AUTO_DELETE_MESSAGE_DURATION"))
-    USER_SESSION_STRING = getConfig("USER_SESSION_STRING")
     TELEGRAM_API = getConfig("TELEGRAM_API")
     TELEGRAM_HASH = getConfig("TELEGRAM_HASH")
 except KeyError as e:
     LOGGER.error("One or more env variables missing! Exiting now")
     exit(1)
+
+LOGGER.info("Generating USER_SESSION_STRING")
+with Client(
+    ":memory:", api_id=int(TELEGRAM_API), api_hash=TELEGRAM_HASH, bot_token=BOT_TOKEN
+) as app:
+    USER_SESSION_STRING = app.export_session_string()
 
 # Stores list of users and chats the bot is authorized to use in
 AUTHORIZED_CHATS = set()
@@ -84,12 +91,11 @@ try:
     with open("authorized_chats.txt", "r+") as f:
         lines = f.readlines()
         for line in lines:
-            #    LOGGER.info(line.split())
             AUTHORIZED_CHATS.add(int(line.split()[0]))
-except OSError:
+except FileNotFoundError:
     pass
 try:
-    for chats in getConfig('AUTHORIZED_CHATS').split(" "):
+    for chats in getConfig("AUTHORIZED_CHATS").split(" "):
         AUTHORIZED_CHATS.add(int(chats))
 except:
     pass
